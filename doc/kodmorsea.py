48433087aea0157d30be5d15bfcdb68adfb67e7c
littlenightingale3 <koszyk63@onet.pl>
2017-02-22 13:31:38 +0100

    dodawanie skryptu

---
+++ doc/kodmorsea.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def koduj(napis):
+    litery = {'a': '*-', 'b': '-***'}
+    for litera in napis:
+        if litera != ' ':
+            print litery[litera]
+
+
+
+def main(args):
+    #pobierz_napis
+    koduj("ab")
+    return 0
+
+
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
