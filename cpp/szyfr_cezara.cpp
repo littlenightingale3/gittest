cf0f67df2ebda3f73f28a63fe796ae637e531618
littlenightingale3 <koszyk63>
2017-03-03 07:56:54 +0100

    szyfr Cezara v1

---
+++ cpp/szyfr_cezara.cpp
@@ -0,0 +1,29 @@
+/*
+ * kod_cezara.cxx
+ *
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main(int argc, char **argv)
+{
+    char tekst[100];
+    int kluzc+ 3;
+    cout << "Poaj tekst: " << endl;
+    cin >> tekst;
+    cout << "Podaj klucz";
+    cin >> klucz;
+    szyfruj(teskt, klucz);
+    cout << tekst;
+
+
+
+	return 0;
+}
+
