b040cf9d85b05e6be6b919d03be2033daf6f5562
kl3ag1 <kl3ag1@komp03.lo1cg.org>
2017-09-08 07:52:13 +0200

    0.8,0.9

---
+++ python/czesc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  czesc.py
+#  
+
+ROK_TERAZ = 2017
+ROK_PYTHON = 1991
+
+def parzyste(n):
+    ile = lista(range(0, n+1, 2)
+    print(ile)
+    return len(ile)
+
+
+def main(args):
+    imie = input("Jak się nazywasz? ")
+    print ("Cześć", imie)
+    wiek = int(input("Ile masz lat "))
+    rok_urodzenia= ROK_TERAZ - wiek
+    print ("Urodziłaś się w ", 2017 - wiek)
+    
+    if rok_urodzenia < ROK_PYTHON;
+        print (" jestem mlodszy ")
+    if rok_urodzenia > ROK_PYTHON;
+      print  ( "jestem starszy")
+      
+      
+      n =int(input("Podaj liczbe: "))
+      print("Parzystych:", parzyste(n)
+      
+      
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
