088c4325583f2171c4bcb9f8172e8a91e3f2bea9
kl3ag1 <kl3ag1@komp03.lo1cg.org>
2017-10-13 07:54:25 +0200

    baza slite3

---
+++ baza_dancyh_sql/zapytania2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  
+#  
+
+import sqlite3
+
+def 
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
