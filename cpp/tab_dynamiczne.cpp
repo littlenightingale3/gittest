8c587f7b39aefe1e043bfd0b0591f0db128dc371
littlenightingale3 <koszyk63>
2018-01-16 14:20:31 +0100

    16,01

--- cpp/tab_dynamiczne.cpp
+++ cpp/tab_dynamiczne.cpp
@@ -7,6 +7,8 @@
 
 
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 
 using namespace std;
 
@@ -39,11 +41,55 @@ int tab1W(){
     return 0;
 }
 
+void wypelnij2W(int **tab, int w, int k) {
+    srand(time(NULL));
+    for(int i =0; i < w; i++){
+        for(int j =0; j < k; j++){
+            //cout << i << j << endl;
+            tab[i][j] = rand() % 11;// dzielenie modulo(reszta z dzielenia, otrzymamy cyfre 100)
+            cout << setw(4) << tab[i][j];
+            
+        }
+        cout << endl;
+    }
+    
+}
+
+
+int tab2W(){
+    int w, k, i;
+    cout << "Ile wierszy i kolumn?";
+    cin >> w >> k;
+    int **tab;// deklaracja wskaznika
+    
+    
+    try{
+    tab = new int *[w]; // zarezerwuj miesjce dla wskaznikow ktore sa wskaznikami do liczb calkowitych
+    } catch(bad_alloc){
+        cout << "Za malo pamieci!";
+    return 1;
+    }
+    
+    
+    for(i=0; i < w;i++){
+        try{
+        tab[i] = new int[k];  // utworzenie tablicy liczb calkowityvh
+        } catch(bad_alloc){
+            cout << "Za malo pamieci!";
+        return 1;
+            }
+        }
+
+    wypelnij2W(tab, w, k);
+    return 0;
+        
+    }
+
 int main(int argc, char **argv)
 {
 	
-    tab1W();
-    
+    tab2W();
+    //tab1W();
     
 	return 0;
 }
