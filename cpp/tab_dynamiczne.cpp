e56d8632aeff8f93342190a8e246843dff2ae06f
littlenightingale3 <koszyk63>
2018-01-18 11:58:09 +0100

    16,01

--- cpp/tab_dynamiczne.cpp
+++ cpp/tab_dynamiczne.cpp
@@ -1,95 +1,84 @@
 /*
  * tab_dynamiczne.cpp
  * 
- * 
- * 
+ * Copyright 2018  <>
+ *  
  */
 
 
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
+#include <iomanip> // setw()
+#include <cstdlib> //srand(), rand()
 
 using namespace std;
 
-void wprowadzDane(int *t, int ile) {
-    for(int i=0; i < ile; i++) {
-        cout << "Podaj liczbe:";
-        //cin >> tab[i];
-        cout << "Adres komorki" << (t + i) << endl;
+void wprowadzDane(int *t, int ile){
+    for (int i = 0; i < ile; i++){
+        cout << "Adres komórki: " << (t + i) <<endl;
+        cout << "Podaj liczbę: ";
+        //cin >> t[i];
         cin >> *(t + i);
     }
 }
 
-
-
 int tab1W(){
-    int ile =0;
-    //tworzenie 1 wymiarowej tablicy dynamicznej
-    cout << "Ile liczb podasz?" << endl;
+    // tworzenie 1-wymiarowej tablicy dynamicznej
+    int ile = 0;
+    cout << "Ile liczb podasz? ";
     cin >> ile;
-    
-    try{
+    try {
         int *tab;
-        tab = new int [ile];
-        wprowadzDane(tab,ile);
-    
-    } catch(bad_alloc){
-        cout << "Za malo pamieci!";
-    return 1;
+        tab = new int[ile];
+        wprowadzDane(tab, ile);
+    } catch(bad_alloc) {
+        cout << "Za mało pamięci!" << endl;
+        return 1;
     }
     return 0;
 }
 
-void wypelnij2W(int **tab, int w, int k) {
-    srand(time(NULL));
-    for(int i =0; i < w; i++){
-        for(int j =0; j < k; j++){
-            //cout << i << j << endl;
-            tab[i][j] = rand() % 11;// dzielenie modulo(reszta z dzielenia, otrzymamy cyfre 100)
-            cout << setw(4) << tab[i][j];
-            
+void wypelnij2W(int **tab, int w, int k){
+    srand(time(NULL)); //inicjacja generatora liczb pseudolosowych
+    for(int i = 0; i < w; i++){
+        for(int j = 0; j < k; j++){
+            tab[i][j] = (i+1) * (j+1);
+            cout << setw(4) << tab [i][j];
         }
         cout << endl;
     }
-    
 }
-
 
 int tab2W(){
     int w, k, i;
-    cout << "Ile wierszy i kolumn?";
+    cout << "Ile wierszy i kolumn: ";
     cin >> w >> k;
-    int **tab;// deklaracja wskaznika
+    int **tab; // deklaracja wskaźnika do wskaźnika
     
-    
-    try{
-    tab = new int *[w]; // zarezerwuj miesjce dla wskaznikow ktore sa wskaznikami do liczb calkowitych
-    } catch(bad_alloc){
-        cout << "Za malo pamieci!";
-    return 1;
+    try {
+        tab = new int *[w]; // utworzenie tablicy wsakźników
+    } catch(bad_alloc) {
+        cout << "Za mało pamięci!" << endl;
+        return 1;
     }
     
-    
-    for(i=0; i < w;i++){
-        try{
-        tab[i] = new int[k];  // utworzenie tablicy liczb calkowityvh
-        } catch(bad_alloc){
-            cout << "Za malo pamieci!";
-        return 1;
-            }
+    for(i = 0; i < w; i++){
+        try {
+            tab[i] = new int[k]; // utworzenie tablicy liczb całkowitych  
+        } catch(bad_alloc) {
+            cout << "Za mało pamięci!" << endl;
+            return 1;
         }
-
+    }
     wypelnij2W(tab, w, k);
     return 0;
-        
-    }
+}
 
 int main(int argc, char **argv)
 {
-	
+    
     tab2W();
-    //tab1W();
+    //~tab1W();
+    
     
 	return 0;
 }
