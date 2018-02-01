36209f557a1d20245b22e7f6beb6839b5ba6bd10
littlenightingale3 <koszyk63>
2018-02-01 13:29:57 +0100

    01.02

---
+++ cpp/lista.cpp
@@ -0,0 +1,84 @@
+/*
+ * lista.cpp
+ */
+
+
+#include <iostream>
+#include "lista.hpp"
+
+Lista::Lista(){
+    head = NULL;
+    tail = NULL;
+    
+}
+
+
+Lista::~Lista(){
+    while(Usun()){;};
+}
+
+
+void Lista::Dodaj(int wartosc){
+        ELEMENT *el = new ELEMENT;//wskaznik do elementu
+        el->wartosc = wartosc;
+        el->nast = NULL;
+        if (head == NULL) {
+            head = el;
+            tail = el;
+        } else {
+            tail -> nast = el;//ustawienie wskaznika nast dotychasowego
+//ostattniego elementu na adres nowego ostatniego elementu 
+            tail = el; //aktualizujemy wskaznik ogon, aby wksazywal na nowy dodany element
+        
+        }
+    
+}
+
+void Lista::Wyswietl() {
+    ELEMENT *el = head;
+    while (el != NULL){
+        std::cout << el -> wartosc << "" ;//using namespace std;
+        el = el -> nast;
+    }
+    std::cout << std::endl;
+    
+}
+
+
+bool Lista::Usun(){
+    if (head != NULL) {
+        if(head == tail){
+            delete head; //usuwamy element nie head ani nie tail
+            head = NULL;
+            tail = NULL;
+        }else{
+                ELEMENT *el = head;
+                while(el->nast != tail){
+                    el = el-> nast; //zapisa adresu przedostatniego elemetnu
+                }
+                delete el->nast;
+                el->nast = NULL;
+                tail = el;
+            }
+            return true;
+        }
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cpp/lista.hpp
@@ -0,0 +1,26 @@
+#ifndef LISTA_HPP
+#define LISTA_HPP
+
+struct ELEMENT{
+  int wartosc;
+  ELEMENT *nast; //wskaznik do nastepnego elementu listy
+};
+
+
+class Lista {
+    private: //zeby z zewnatrz nie tykac sie tych wartosci pisze sie private  (hermetyzacja danych)
+        ELEMENT *head;
+        ELEMENT *tail;
+        
+    public: //interfejs publiczny (API klasy)
+        Lista(); //konstrkutor
+        ~Lista(); //destruktor 
+        //memory leaks - wycieki pamieci(programy nie informuja systemu ze pamiec ktora wykorzystywal nie jest im juz potrzebna
+        void Dodaj(int);
+        void Wyswietl();
+        bool Usun();
+        
+    
+};
+
+#endif
